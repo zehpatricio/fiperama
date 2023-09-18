529fdfbf3c6bb7db6a5938d9c5e39cd25519b3a6
José Patrício <jpegx100@gmail.com>
2023-09-18 09:46:04 -0300

    #5 proccess brands queue

--- tests/unity/test_import_cars_service.py
+++ tests/unity/test_import_cars_service.py
@@ -29,13 +29,15 @@ def service(mock_fipe_repository, mock_queue_repository, mock_cars_repository):
     return service
 
 
-def test_call_method(service, mock_fipe_repository, mock_queue_repository):
-    mock_data = [
-        {'codigo': '5585', 'nome': 'AMAROK CD2.0 16V/S CD2.0 16V TDI 4x2 Die'},
-        {'codigo': '5586', 'nome': 'AMAROK CD2.0 16V/S CD2.0 16V TDI 4x4 Die'}
-    ]
+def test_call_method(service, mock_fipe_repository, mock_cars_repository):
+    car_1 = {
+        'codigo': '5585', 
+        'nome': 'AMAROK CD2.0 16V/S CD2.0 16V TDI 4x2 Die'
+    }
+    mock_data = [car_1]
     mock_fipe_repository.fetch_cars.return_value = mock_data
 
     service.import_cars('{"codigo": "163"}')
 
     mock_fipe_repository.fetch_cars.assert_called_once_with('163')
+    mock_cars_repository.insert.assert_called_with(car_1)
